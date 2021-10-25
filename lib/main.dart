6e06c1d79911ab50fbe73c40b4ef03713d9eca68
afcking <owner@afcking.com>
2021-10-24 23:44:59 -0600

    app bar app body imge attacth abd text colour

--- lib/main.dart
+++ lib/main.dart
@@ -2,34 +2,71 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyBook());
-}
 
+}
+String imagesrc1=("https://p0.pxfuel.com/preview/853/319/641/flower-nature-summer-plant.jpg");
 class MyBook extends StatelessWidget {
   const MyBook({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: BookHomePage(),
+      home: HomePage(),
     );
-
   }
 }
-class BookHomePage extends StatefulWidget {
-  const BookHomePage({Key? key}) : super(key: key);
+
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
 
   @override
-  _BookHomePageState createState() => _BookHomePageState();
+  _HomePageState createState() => _HomePageState();
 }
 
-class _BookHomePageState extends State<BookHomePage> {
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     appBar:AppBar(
-      title: Text("Bismillah"),
-     ) ,
+    double height=MediaQuery.of(context).size.height;
+    double width= MediaQuery.of(context).size.width;
+
+      return Scaffold(
+        appBar: AppBar(
+          centerTitle: true,
+          title: Text("Faysal App",style:TextStyle
+            (
+            fontSize: 25,fontStyle: FontStyle.normal,fontWeight: FontWeight.bold,color: Colors.red,backgroundColor: Colors.white,
+          ),),
+        ),
+        body: Center(
+          child: Container(
+            height: height,
+            width: width,
+
+            color: Colors.blueAccent,
+            child: Column(
+              
+              children: [
+
+                Container(
+                  width: width,
+                  alignment: Alignment.center,
+                  color: Colors.yellow,
+                  child: Text("give me your Pasword",style:TextStyle(
+                    fontSize: 45,fontWeight: FontWeight.bold,fontStyle:
+                      FontStyle.italic,color: Colors.indigoAccent,
+                  ),),
+
+                ),
+
+                Container(
+                  height: height/2,
+                    width: width/2,
+                    child: Image.network(imagesrc1)),
+              ],
+            ),
+          ),
+        ),
+
     );
   }
 }
-
