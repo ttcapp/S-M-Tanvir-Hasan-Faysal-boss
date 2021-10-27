886d91a5945ff7b0b3bd8df9911536d213281c06
afcking <owner@afcking.com>
2021-10-27 00:38:58 -0600

    assist photo add

--- lib/main.dart
+++ lib/main.dart
@@ -1,72 +1,55 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(MyBook());
-
 }
-String imagesrc1=("https://p0.pxfuel.com/preview/853/319/641/flower-nature-summer-plant.jpg");
+
 class MyBook extends StatelessWidget {
   const MyBook({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: HomePage(),
+      home: Homepage(
+      ),
     );
   }
 }
-
-class HomePage extends StatefulWidget {
-  const HomePage({Key? key}) : super(key: key);
+class Homepage extends StatefulWidget {
+  const Homepage({Key? key}) : super(key: key);
 
   @override
-  _HomePageState createState() => _HomePageState();
+  _HomepageState createState() => _HomepageState();
 }
 
-class _HomePageState extends State<HomePage> {
+class _HomepageState extends State<Homepage> {
   @override
   Widget build(BuildContext context) {
-    double height=MediaQuery.of(context).size.height;
-    double width= MediaQuery.of(context).size.width;
+    return Scaffold
+      (appBar: AppBar(
+      centerTitle: true,
+      title: Text("Facebook"),backgroundColor: Colors.blueAccent,
+    ),
+      body:
 
-      return Scaffold(
-        appBar: AppBar(
-          centerTitle: true,
-          title: Text("Faysal App",style:TextStyle
-            (
-            fontSize: 25,fontStyle: FontStyle.normal,fontWeight: FontWeight.bold,color: Colors.red,backgroundColor: Colors.white,
-          ),),
-        ),
-        body: Center(
-          child: Container(
-            height: height,
-            width: width,
 
-            color: Colors.blueAccent,
-            child: Column(
-              
-              children: [
+      Container(
+        child: SingleChildScrollView(
+          child: Column(
 
-                Container(
-                  width: width,
-                  alignment: Alignment.center,
-                  color: Colors.yellow,
-                  child: Text("give me your Pasword",style:TextStyle(
-                    fontSize: 45,fontWeight: FontWeight.bold,fontStyle:
-                      FontStyle.italic,color: Colors.indigoAccent,
-                  ),),
+            children: [ Image.asset("assets/image/image.jpg"),
+              Image.asset("assets/image/img.png.jpg"),
+              Image.asset("assets/image/imagell.jpg"),
 
-                ),
 
-                Container(
-                  height: height/2,
-                    width: width/2,
-                    child: Image.network(imagesrc1)),
-              ],
-            ),
+              Text("give me password",style:TextStyle(fontStyle: FontStyle.normal,fontSize: 25,fontWeight: FontWeight.bold)
+                ,)
+            ],
           ),
         ),
 
+      ) ,
     );
   }
 }
